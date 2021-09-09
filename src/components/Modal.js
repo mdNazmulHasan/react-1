22004c6f5cf3a650d05bab6c1111b1f1d33603e9
mdNazmulHasan <najmul.hasan10@gmail.com>
2021-09-09 14:09:28 +0600

    basic function passed as prop

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Modal(props) {
+    return (
+        <div className="modal">
+        <p>Are you sure?</p>
+        <button className="btn btn--alt" onClick={props.closeModalHandler}>Cancel</button>
+        <button className="btn" onClick={props.closeModalHandler}>Confirm</button>
+        </div>
+    )
+}
